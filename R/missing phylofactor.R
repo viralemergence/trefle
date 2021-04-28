d5095f350fee24521b05ce5493e7f16e5bde5a0e
Daniel Becker <dbecker@uga.edu>
2021-04-28 14:28:12 -0400

    finalized gpf

--- R/missing phylofactor.R
+++ R/missing phylofactor.R
@@ -1,5 +1,6 @@
-## trefle phylofactor
+## trefle phylofactor of missing viruses
 ## danbeck@ou.edu
+## last update 4/28/2021
 
 ## clean
 rm(list=ls()) 
@@ -38,10 +39,26 @@ full_join(clo, tre) %>%
 
 ## reorganize
 df %<>% dplyr::mutate(Missing = as.numeric((Trefle-Clover)==1))
-df %<>% group_by(Host) %>% summarize(Missing = sum(Missing))
+
+## aggregate missing
+df1=df
+df1 %<>% group_by(Host) %>% summarize(Missing = sum(Missing))
+
+## aggregate clover
+df2=df
+df2 %<>% group_by(Host) %>% summarize(Clover = sum(Clover))
+
+## combine
+df=merge(df1,df2,by="Host")
+rm(df1,df2)
 
 ## clean
 rm(clo,tre)
+
+## visualize
+ggplot(df,aes(log1p(Clover),log1p(Missing)))+
+  geom_point(alpha=0.5)+
+  geom_smooth(method="gam",formula=y~s(x,bs="ts"))
 
 ## load Upham phylogeny
 setwd("~/Desktop/clover/data/phylogenies")
@@ -68,6 +85,16 @@ taxa=read_excel('masterTaxonomy.xlsx',3)
 taxa$tips=taxa$SciName
 taxa$tips=sapply(strsplit(taxa$tips,'_'),function(x) paste(x[1],x[2],sep=' '))
 
+## uppercase first
+firstup <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
+
+## fix
+taxa$Order=firstup(tolower(taxa$Order))
+taxa$Family=firstup(tolower(taxa$Family))
+
 ## trim
 taxa=data.frame(taxa[c("tips","Order","Family","Genus")])
 
@@ -79,6 +106,7 @@ rm(taxa)
 dups=table(df$tips)>1
 dups=names(which(dups))
 dups=df[df$tips%in%dups,]
+length(unique(dups$tips))
 
 ## remove rows from df
 df=df[!df$tips%in%dups$tips,]
@@ -86,10 +114,15 @@ df=df[!df$tips%in%dups$tips,]
 ## save metadata
 meta=dups[!duplicated(dups$tips),]
 meta$Missing=NULL
+meta$Clover=NULL
 
 ## aggregate
-dups=aggregate(Missing~tips,data=dups,mean)
+dups1=aggregate(Missing~tips,data=dups,mean)
+dups2=aggregate(Clover~tips,data=dups,mean)
+dups=merge(dups1,dups2,by="tips")
+rm(dups1,dups2)
 dups$Missing=round(dups$Missing,0)
+dups$Clover=round(dups$Clover,0)
 
 ## merge with metadata
 dups=merge(dups,meta,by="tips",all.x=T)
@@ -120,6 +153,10 @@ cdata=comparative.data(phy=ptree,data=df,names.col=label,vcv=T,na.omit=F,warn.dr
 ## taxonomy
 cdata$data$Species=cdata$data$tips
 cdata$data$taxonomy=with(cdata$data,paste(Order,Family,Genus,Species,sep="; "))
+
+## pgls lambda
+# mod=pgls(Missing~1,data=cdata,lambda="ML")
+# summary(mod)
 
 ## save tree
 cdata$data$label=rownames(cdata$data)
@@ -185,10 +222,11 @@ obj.fcn2 <- function(fit,grp,tree,PartitioningVariables,model.fcn,phyloData,...)
 set.seed(1)
 pf=gpf(Data=cdata$data,tree=cdata$phy,
             frmla.phylo=Missing~phylo,
-            model.fcn = model.fcn2,objective.fcn = obj.fcn2,
+            model.fcn = model.fcn2,
+            objective.fcn = obj.fcn2,
             cluster.depends='library(MASS)',
             algorithm='phylo',nfactors=5,
-            min.group.size=5)
+            min.group.size=10)
 
 ## Holm procedure
 hp=HolmProcedure(pf)
@@ -213,7 +251,7 @@ colnames(results)=c('factor','taxa','tips','node',"clade",'other')
 ## set taxonomy
 taxonomy=dat[c('Species','taxonomy')]
 taxonomy$taxonomy=as.character(taxonomy$taxonomy)
-  
+
 ## loop
 for(i in 1:hp){
   
@@ -239,6 +277,9 @@ for(i in 1:hp){
   
   ## get means
   ms=(tapply(dat[,"Missing"],dat[,paste0("Missing",'_pf',i)],mean,na.rm=T))
+  
+  ## round
+  ms=round(ms,0)
   
   ## add in
   results[i,'clade']=ms['factor']
@@ -290,5 +331,46 @@ gg=gg+
 
 ## export
 setwd("~/Desktop/trefle/figures")
-
+png("phylofactor-missing.png",width=5,height=5,units="in",res=600)
 gg
+dev.off()
+
+## clean table
+results2=results
+results2$node=NULL
+write.csv(results2,"ED table phylofactor.csv")
+
+## organize clade
+dat$pf=ifelse(dat$Missing_pf1=="factor","1",
+              ifelse(dat$Missing_pf2=="factor","2",
+                     ifelse(dat$Missing_pf3=="factor","3",
+                            ifelse(dat$Missing_pf4=="factor","4","other"))))
+dat$pf=factor(dat$pf)
+
+## separate
+dat2=dat[dat$pf=="other",]
+dat3=dat[!dat$pf=="other",]
+
+## plot scaling
+ggplot(dat,aes(log1p(Clover),Missing))+
+  
+  ## plot nonpf points first
+  geom_point(data=dat2,alpha=0.5,colour="grey80")+
+
+  ## gam
+  geom_smooth(method="gam",formula=y~s(x,bs="ts"),colour="black",alpha=0.5)+
+  
+  ## plot pf
+  geom_point(data=dat3,alpha=0.75,size=2,aes(colour=pf))+
+  scale_colour_viridis_d()+
+  scale_fill_viridis_d()+
+  
+  ## pf smooths
+  geom_smooth(data=dat3,method="gam",formula=y~s(x,bs="ts",k=5),
+              aes(group=pf,colour=pf,fill=pf),alpha=0.25)+
+  
+  ## theme
+  theme_bw()+
+  theme(legend.position="top")+
+  guides(colour=guide_legend(title="phylofactor clade"),
+         fill=guide_legend(title="phylofactor clade"))
